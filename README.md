dd6b7cf305d15a5cd5a6323db0b40ebfdcd3b4d1
robot <black.holl@bk.ru>
2019-02-23 15:41:09 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# My personal config GNU Emacs
+## Installation
+```bash
+git clone git@github.com:lancer55555/.emacs.d.git
+```
---
+++ init.el
@@ -0,0 +1,32 @@
+;;; init.el --- main configuration file
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+;; Bootstrap `use-package'
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(org-babel-load-file (expand-file-name "~/.emacs.d/settings.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (ivy avy org-bullets which-key try use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;;; init.el ends here
