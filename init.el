9974892d132d84c37feee599089b71f4661cb54e
lanser55555 <black.holl@bk.ru>
2019-03-14 17:14:12 +0500

    settings switch-window

--- init.el
+++ init.el
@@ -31,7 +31,7 @@
     ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
  '(package-selected-packages
    (quote
-    (rainbow-mode avy beacon which-key spacemacs-theme use-package))))
+    (switch-window rainbow-mode avy beacon which-key spacemacs-theme use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
