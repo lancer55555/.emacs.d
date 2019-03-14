d05e7b49144bb69d895b93677f8d60cef5321f85
lanser55555 <black.holl@bk.ru>
2019-03-14 15:39:05 +0500

    add avy

--- init.el
+++ init.el
@@ -29,7 +29,7 @@
  '(custom-safe-themes
    (quote
     ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
- '(package-selected-packages (quote (beacon which-key spacemacs-theme use-package))))
+ '(package-selected-packages (quote (avy beacon which-key spacemacs-theme use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
