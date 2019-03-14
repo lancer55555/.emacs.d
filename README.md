72c7ad87d14854f4d444c8ea76ae36cfabe774ba
lanser55555 <black.holl@bk.ru>
2019-03-14 12:29:19 +0500

    fixing and go

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# My personal config GNU Emacs
+# My personal config GNU Emacs.
 ## Installation
 ```bash
-git clone git@github.com:lancer55555/.emacs.d.git
+git clone git@github.com:lancer55555/.emacs.d.git ~/.emacs.d/
 ```
--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-;;; init.el --- main configuration file
+;; init.el --- main configuration file
 
 ;;; Commentary:
 
@@ -10,18 +10,38 @@
 	     '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
-;; Bootstrap `use-package'
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 
-(org-babel-load-file (expand-file-name "~/.emacs.d/settings.org"))
+(unless (package-installed-p 'spacemacs-theme)
+  (package-refresh-contents)
+  (package-install 'spacemacs-theme))
+
+(use-package which-key
+	     :ensure t
+	     :init
+	     (which-key-mode))
+
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(blink-cursor-mode -1)
+(setq make-backup-files nil)
+(setq auto-save-default nil)
+(setq auto-save-list-file-name nil)
+
+(setq inhibit-startup-message t)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (ivy avy org-bullets which-key try use-package))))
+ '(custom-enabled-themes (quote (spacemacs-dark)))
+ '(custom-safe-themes
+   (quote
+    ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
+ '(package-selected-packages (quote (which-key spacemacs-theme use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
