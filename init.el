137642ecb5681fb08645c1fdf4c093df9c0853fd
lanser55555 <black.holl@bk.ru>
2019-03-14 16:42:37 +0500

    add rainbow

--- init.el
+++ init.el
@@ -29,7 +29,9 @@
  '(custom-safe-themes
    (quote
     ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
- '(package-selected-packages (quote (avy beacon which-key spacemacs-theme use-package))))
+ '(package-selected-packages
+   (quote
+    (rainbow-mode avy beacon which-key spacemacs-theme use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
